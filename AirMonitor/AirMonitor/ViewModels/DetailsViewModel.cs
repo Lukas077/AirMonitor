b7d4a1261a87d13a6897d89286813fa55844889e
Lukas077 <lukaszklosek@microsoft.wsei.edu.pl>
2020-09-08 23:43:45 +0200

    Bindowanie w DetailsPage

--- AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
+++ AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
@@ -17,9 +17,86 @@ namespace AirMonitor.ViewModels
 
         }
 
+        string caqiText = "Świetna jakość!";
+        public string CaqiText
+        {
+            get => caqiText;
+            set => SetProperty(ref caqiText, value);
+
+        }
+
+        string caqiDesc = "Możesz bezpiecznie wyjść z domu bez swojej maski anty-smogowej i nie bać się o swoje zdrowie.";
+        public string CaqiDesc
+        {
+            get => caqiDesc;
+            set => SetProperty(ref caqiDesc, value);
+
+        }
+
+        int pmValue25 = 34;
+        public int PmValue25
+        {
+            get => pmValue25;
+            set => SetProperty(ref pmValue25, value);
+
+        }
+
+        int pmPercent25 = 137;
+        public int PmPercent25
+        {
+            get => pmPercent25;
+            set => SetProperty(ref pmPercent25, value);
+
+        }
+
+        int pmValue10 = 67;
+        public int PmValue10
+        {
+            get => pmValue10;
+            set => SetProperty(ref pmValue10, value);
+
+        }
+
+        int pmPercent10 = 135;
+        public int PmPercent10
+        {
+            get => pmPercent10;
+            set => SetProperty(ref pmPercent10, value);
+
+        }
+
+        double humidity = 0.65;
+        public double Humidity
+        {
+            get => humidity;
+            set => SetProperty(ref humidity, value);
+
+        }
+
+        
+
+        int pressure = 1000;
+        public int Pressure
+        {
+            get => pressure;
+            set => SetProperty(ref pressure, value);
+
+        }
+
+
+
+
+
+        private void SetProperty(ref string caqiText, string value)
+        {
+        }
+
         private void SetProperty(ref int caqiValue, int value)
         {
-            throw new NotImplementedException();
+        }
+
+        private void SetProperty(ref double humidity, double value)
+        {
         }
     }
 
