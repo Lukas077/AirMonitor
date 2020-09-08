d210ddb097df5aec1ce68ba464a1db99eefda1c4
Lukas077 <lukaszklosek@microsoft.wsei.edu.pl>
2020-09-08 20:56:17 +0200

    Wywołanie wartości Text przez DetailsViewModel

---
+++ AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AirMonitor.ViewModels
+{
+    class DetailsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        int caqiValue = 50;
+        public int CaqiValue
+        {
+            get => caqiValue;
+            set => SetProperty(ref caqiValue, value);
+
+        }
+
+        private void SetProperty(ref int caqiValue, int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    
+
+}
