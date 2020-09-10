0441a63c6aad7c9be561b7bd948bacaea515c16a
Lukas077 <lukaszklosek@microsoft.wsei.edu.pl>
2020-09-10 19:28:49 +0200

    Stworzenie modeli

---
+++ AirMonitor/AirMonitor/Models/Measurement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirMonitor.Models
+{
+    class Measurement
+    {
+        public int CurrentDisplayValue { get; set; }
+        public MeasurementItem Current { get; set; }
+        public MeasurementItem[] History { get; set; }
+        public MeasurementItem[] Forecast { get; set; }
+        public Nearest Installation { get; set; }
+    }
+}
